9a09d5b16172c3413e27707661414dc6defd6611
Igor <igoroshust75@gmail.com>
2024-12-04 15:51:37 +0900

    feat: owlcarousel

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    const owl = $('.owl-carousel');
+    owl.owlCarousel({
+        center: true,
+        loop: true,
+        margin: 30,
+        startPosition: 1,
+        items: 3,
+    });
+
+    $('.slider__btn--prev').click(function() {
+        owl.trigger('prev.owl.carousel');
+    });
+
+    $('.slider__btn--next').click(function() {
+        owl.trigger('next.owl.carousel');
+    });
+});
